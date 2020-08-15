d9b3e096322e51b7d8f13cbc018ce53a39a6b513
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-08-15 15:49:04 +0900

    build: setting rollup

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+
+const packageJson = require("./package.json");
+
+/** @type import('rollup').RollupOptions[] */
+const options = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      typescript({
+        incremental: false,
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "./declaration/src/index.d.ts",
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "es",
+      },
+    ],
+    plugins: [dts()],
+  },
+];
+
+export default options;
--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
 
+export type TextProps = {
+  text: string
+}
 
-export const Text = () => <div>text</div>
+export const Text = (props: TextProps) => <div>{props.text}</div>
