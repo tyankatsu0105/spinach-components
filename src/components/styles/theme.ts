1c30a5854b1cb6f2843e3f801104951da69dd535
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-02-15 21:14:42 +0900

    feat: palette
    false
    
    false

--- src/components/styles/theme.ts
+++ src/components/styles/theme.ts
@@ -1,14 +1,4 @@
+import { palettes } from './palettes';
 export const theme = {
-  colors: {
-    powderWhite: '#FFFDF9',
-    persianGreen: '#06B49A',
-    lightBlue: '#AFDBD2',
-    onyx: '#36313D',
-  },
-  fonts: ['sans-serif', 'Roboto'],
-  fontSizes: {
-    small: '1em',
-    medium: '2em',
-    large: '3em',
-  },
+  palettes,
 } as const;
---
+++ src/components/theme/palette.stories.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { filename } from 'paths.macro';
+
+import { storybookMeta } from '../../shared/helpers';
+
+import { theme } from '../styles/theme';
+
+export default storybookMeta({
+  filename,
+  group: 'Theme',
+});
+
+export const Palettes = (): React.ReactElement => (
+  <ul>
+    {Object.entries(theme.palettes).map(([paletteKey, paletteValues]) => (
+      <li key={paletteKey} className="mb-10">
+        <p className="text-2xl">{paletteKey}</p>
+        <ul className="flex">
+          {Object.entries(paletteValues).map(([colorKey, colorValue]) => (
+            <li key={colorKey}>
+              <PalleteColorItem color={colorValue}></PalleteColorItem>
+              {colorKey}
+            </li>
+          ))}
+        </ul>
+      </li>
+    ))}
+  </ul>
+);
+
+const PalleteColorItem = styled.div<{ color: string }>`
+  width: 50px;
+  height: 50px;
+  background-color: ${(props) => props.color};
+`;
